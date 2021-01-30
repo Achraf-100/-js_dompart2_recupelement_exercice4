fa7c03406d012726ae2360ae6756a6f9d9d1baf6
Achraf <hardy1348@live.fr>
2021-01-30 15:55:17 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,30 @@
+let recup = document.getElementsByClassName('redPurple')
+console.log(recup);
+
+
+
+let recup1 = document.querySelectorAll('.redPurple')
+console.log(recup1);
+
+
+
+let recup2 = document.querySelectorAll('h1.redPurple')
+console.log(recup2);
+
+
+
+
+let recup3 = document.querySelectorAll('li,p,span')
+console.log(recup3);
+
+
+
+
+let recup4 = document.querySelectorAll('li.important, p')
+console.log(recup4);
+
+
+
+
+let recup5 = document.querySelectorAll('h1.redPurple, span.redPurple')
+console.log(recup5);
